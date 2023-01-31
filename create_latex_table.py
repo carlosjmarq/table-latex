a0204c7fc4ad85cd404c5543d1f1fdd7f390611a
carlosjmarq <carlosjmarq@gmail.com>
2023-01-31 17:49:04 -0400

    feat: adding caption

--- create_latex_table.py
+++ create_latex_table.py
@@ -2,7 +2,7 @@ import os
 from typing import Any, Dict, List, Optional, Union
 import pathlib
 
-def create_table (data: Dict[str, List[Any]]) -> Optional[str]:
+def create_table (data: Dict[str, List[Any]], caption: Optional[str]) -> Optional[str]:
 	columns = data.keys()
 	max_index = max([len(data[column]) for column in columns])
 	if not max_index: return "not valid"
@@ -20,7 +20,7 @@ def create_table (data: Dict[str, List[Any]]) -> Optional[str]:
 		table.write("		" + " & ".join([str(data[column][i]) if i < len(data[column]) else "" for column in columns]) + "\\\\" + os.linesep)
 	table.write("		\\hline" + os.linesep)
 	table.write("	\\end{tabular}" + os.linesep)
-	table.write("	\\caption{Tabla generada en python.}" + os.linesep)
+	table.write("	\\caption{%s}" % (caption or "Tabla generada en python.") + os.linesep)
 	table.write("	\\label{tab:pythonTab}" + os.linesep)
 	table.write("\\end{table}" + os.linesep)
 
@@ -28,12 +28,12 @@ def create_table (data: Dict[str, List[Any]]) -> Optional[str]:
 	return None
 
 
-dummy_data = {
-	"col1": [1,2,3,4,5,6],
-	"col2": [2,4,6,8,10,12],
-	"col3": [3,6,9,12,15,18],
-	"col4": [4,5,6,7],
-	"col5": [0,0,0,0,0,0]
-}
+# dummy_data = {
+# 	"col1": [1,2,3,4,5,6],
+# 	"col2": [2,4,6,8,10,12],
+# 	"col3": [3,6,9,12,15,18],
+# 	"col4": [4,5,6,7],
+# 	"col5": [0,0,0,0,0,0]
+# }
 
-create_table(dummy_data)
+# create_table(dummy_data)
